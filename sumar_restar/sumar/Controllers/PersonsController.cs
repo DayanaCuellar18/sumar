4c2c8dbafd25a59553f18ac7a8ccd6ee67bb7fb6
Dayana Nicole Cuellar Gonzales <a2018117131@estudiantes.upsa.edu.bo>
2020-08-15 07:32:15 -1200

    Primer modelo

---
+++ sumar_restar/sumar/Controllers/PersonsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using sumar.Models;
+
+namespace sumar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        [HttpGet]
+        public Person listar()
+        {
+            Person person = new Person { PersonID = 1, Name = "Dayana" };
+            return person;
+        }
+    }
+}
--- sumar_restar/sumar/Controllers/sumaController.cs
+++ sumar_restar/sumar/Controllers/sumaController.cs
@@ -12,14 +12,28 @@ namespace sumar.Controllers
     public class sumaController : ControllerBase
     {
         [HttpGet]
+        [Route("Add")]
         public int Add (int a, int b)
         {
             return a + b;
         }
+        [HttpGet]
+        [Route("Multiply")]
+        public int Mult(int a, int b)
+        {
+            return a * b;
+        }
         [HttpPost]
+        [Route("Add")]
         public int Add2([FromHeader] int a, [FromHeader] int b)
         {
             return a + b;
+        }
+        [HttpPost]
+        [Route("Multiply")]
+        public int Mult2([FromHeader] int a, [FromHeader] int b)
+        {
+            return a * b;
         }
     }
 }
